ebf8e0ea65a55b3fa903475092d30d375c6dc090
Indra jaya kesuma <162561101+IndraKnz@users.noreply.github.com>
2024-10-15 08:56:06 -0700

    Add files via upload

---
+++ tamu.sql
@@ -0,0 +1,68 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: localhost:3306
+-- Generation Time: Oct 15, 2024 at 07:09 PM
+-- Server version: 10.5.25-MariaDB-cll-lve
+-- PHP Version: 8.1.29
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `petd2575_apel3`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tamu`
+--
+
+CREATE TABLE `tamu` (
+  `id` int(11) NOT NULL,
+  `nama` varchar(100) NOT NULL,
+  `email` varchar(100) NOT NULL,
+  `pesan` text NOT NULL,
+  `tanggal` timestamp NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+
+--
+-- Dumping data for table `tamu`
+--
+
+INSERT INTO `tamu` (`id`, `nama`, `email`, `pesan`, `tanggal`) VALUES
+(1, 'testing', 'testing@admin.com', 'testing\r\n', '2024-10-14 00:17:52');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `tamu`
+--
+ALTER TABLE `tamu`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `tamu`
+--
+ALTER TABLE `tamu`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
